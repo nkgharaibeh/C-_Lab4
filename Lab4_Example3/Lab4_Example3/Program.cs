4213f936173150d1ab304e053699c9259b3a1665
Natheer Khlaif Gharaibeh <nkgharaibeh@gmail.com>
2025-11-26 12:00:22 +0300

    Add files via upload
    استخدام Properties

---
+++ Lab4_Example3/Lab4_Example3/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Lab4_Example3
+{
+    class Person
+    {
+        private string name;
+        private int age;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public int Age
+        {
+            get { return age; }
+            set
+            {
+                if (value >= 0)
+                    age = value;
+                else
+                    Console.WriteLine("Age must be positive!");
+            }
+        }
+
+        public void DisplayInfo()
+        {
+            Console.WriteLine($"Name: {Name}, Age: {Age}");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p = new Person();
+            p.Name = "Rania";
+            p.Age = 30;
+            p.DisplayInfo();
+
+            p.Age = -3; // سيطبع رسالة خطأ
+            Console.ReadLine();
+        }
+    }
+}
