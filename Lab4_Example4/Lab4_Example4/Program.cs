9c5ccaabf84bebefaa335fad957a41053dd9d6f8
Natheer Khlaif Gharaibeh <nkgharaibeh@gmail.com>
2025-11-26 12:01:44 +0300

    Add files via upload
    Auto-Implemented Properties

---
+++ Lab4_Example4/Lab4_Example4/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Lab4_Example4
+{
+    class Car
+    {
+        public string Brand { get; set; }
+        public double Price { get; set; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car c = new Car
+            {
+                Brand = "Toyota",
+                Price = 15000
+            };
+
+            Console.WriteLine($"Brand: {c.Brand}, Price: {c.Price}");
+            Console.ReadLine();
+        }
+    }
+}
