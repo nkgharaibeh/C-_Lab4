ed4ef5c3836ff54be3612b8ec3c4d10121a83919
Natheer Khlaif Gharaibeh <nkgharaibeh@gmail.com>
2025-12-15 20:35:32 +0300

    without Encapsulation

--- Example1.cs
+++ Example1.cs
@@ -4,6 +4,7 @@ namespace Lab4_Example1
 {
     class Person
     {
+        // without Encapsulation (fields are public)
         public string Name;
         public int Age;
     }
@@ -15,7 +16,7 @@ namespace Lab4_Example1
             Person p = new Person();
             p.Name = "Ahmad";
             p.Age = -5;   // قيمة غير منطقية – هنا المشكلة
-
+//هذا المثال يبيّن المشكلة: نستطيع وضع Age = -5 بسهولة → انتهاك لسلامة البيانات.
             Console.WriteLine($"Name: {p.Name}, Age: {p.Age}");
             Console.ReadLine();
         }
